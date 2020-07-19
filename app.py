03e1d4b290e1f30b9c7ab8df74209074d8a73e49
jang <jangiwalia@yahoo.com>
2020-07-19 08:33:35 -0700

    First route to homepage

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "This is about me page"
+@app.route("/map")
+def map():
+    return render_template("map.html")
 
-@app.route("/contact")
-def contact():
-    return "This is Contact Us page"
+@app.route("/apidata")
+def api():
+    return render_template('api.html')
+
+@app.route("/csvdata")
+def csv():
+    return render_template('csv.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
